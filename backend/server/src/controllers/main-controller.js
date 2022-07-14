089bb3dc021cff0ad05585c635c45ecb433b03af
juanfranflores <juan27fp@gmail.com>
2022-07-14 17:37:25 -0300

    muestro boton de crear proyecto si soy empresa

--- backend/server/src/controllers/main-controller.js
+++ backend/server/src/controllers/main-controller.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const path = require('path');
 const db = require('../../database/models');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
