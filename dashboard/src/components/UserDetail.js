63006ff14c067a1204e54fd03030324de7ef7d20
ramiro-redona <ramiroredona@gmail.com>
2022-08-01 19:15:28 -0300

    react

--- dashboard/src/components/UserDetail.js
+++ dashboard/src/components/UserDetail.js
@@ -1,41 +1,66 @@
 import React, { Component } from "react";
-import UsersInDbCard from "./UsersInDbCard"
+import { useParams } from "react-router-dom"
 
 class UserDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: null,
+      name: null,
+      username: null,
+      email: null,
       bgColor: "",
     };
   }
 
   render() {
-    if (!this.state.user) {
+    /* if (!this.state.user) {
       return "Cargando...";
-    }
+    } */
     return (
-      <UsersInDbCard title="Detalle Usuario"> 
-        <h3>{this.state.user.name}</h3>
+      <div>
         <div className="text-center">
         </div>
         <p>
-          {this.state.user}
+          {this.state.name}
         </p>
-      </UsersInDbCard>
+        <p>
+          {this.state.username}
+        </p>
+        <p>
+          {this.state.email}
+        </p>
+      </div>
     );
   }
 
   async componentDidMount() {
     const response = await fetch("http://localhost:3000/api/userApiRouter");
     const usersListData = await response.json();
-    console.log('usuarioss', usersListData)
 
+    console.log('usuarioss', usersListData)
+    const users = usersListData.data;
+    console.log('userrsss', users)
+    const { id } = this.props.match.params;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id == id) {
+        this.setState({
+          name: users[i].name,
+        });
+        this.setState({
+          username: users[i].username,
+        });
+        this.setState({
+          email: users[i].email,
+        });
+      }
+    }
     this.setState({
       users: usersListData.data,
     });
+
   }
-  
+
 }
 
 export default UserDetail;
--- dashboard/src/components/UsersInDBCard.js
+++ dashboard/src/components/UsersInDBCard.js
@@ -1,11 +1,16 @@
 // userInDb
 import BigCard from "./BigCard";
 import UsersInDb from "./UsersInDb";
+import UsersDetail from "./UserDetail";
+import { Route, Switch } from "react-router-dom";
 
 export default function UsersInDbCard() {
   return (
     <BigCard title=" in database">
       <UsersInDb />
+      <Switch>
+        <Route path="/users/:id" exact component={UsersDetail} />
+      </Switch>
     </BigCard>
   );
 }
